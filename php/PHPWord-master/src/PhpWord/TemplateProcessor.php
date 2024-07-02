e1703f9b10f3089ad210e4f1c967c89527c294f6
Starkiller86 <156952869+Starkiller86@users.noreply.github.com>
2024-07-02 10:28:32 -0600

    add specifically route of vendor

--- php/PHPWord-master/src/PhpWord/TemplateProcessor.php
+++ php/PHPWord-master/src/PhpWord/TemplateProcessor.php
@@ -17,7 +17,7 @@
 
 namespace PhpOffice\PhpWord;
 
-require_once 'vendor/autoload.php';
+require_once '../php/PHPWord-master/vendor/composer/autoload.php';
 
 use PhpOffice\PhpWord\Exception\CopyFileException;
 use PhpOffice\PhpWord\Exception\CreateTemporaryFileException;
